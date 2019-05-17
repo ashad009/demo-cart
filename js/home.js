43d66806b503878a898389d706262fd8d2d7e2f2
ashad009 <35979259+ashad009@users.noreply.github.com>
2019-05-17 12:34:58 +0200

    Generate cart view

--- js/home.js
+++ js/home.js
@@ -13,15 +13,33 @@ export class Home {
     }
 
     getItems = () => {
-        console.log(this.webService);
         this.webService.getItmes()
             .then((data) => {
                 this.items = data;
+                let view = this.generateView(this.items);
+                document.getElementById("items").innerHTML = view;
+                console.log('Then here',view);
             })
             .catch(err => {
                 err.text().then(errorMessage => {
                     console.error(errorMessage);
                 })
-            })
+            });
+    }
+
+    generateView = (data) =>{
+        console.log('Generate View',data)
+        let view =`
+        <h2>Select your items</h2>
+        ${data.map((item)=>{
+            return `
+            <div> ${item.name} </div>
+            <div> ${item.price} </div>
+            <div> ${item.quantity} </div>
+            <button class = "addbtn" id = "${item.id}" name = "${item.name}" price = "${item.price}" quantity = "${item.quantity}">Add Item</button>
+           `;
+        }).join('')}
+        `;
+        return view;
     }
 }
